7e6638744422718349fb590bc58b985f5fb8f45b
Anuj Jhawar <anujjhawar28@gmail.com>
2025-07-19 02:11:07 +0530

    Fixed the Border Color On Selection and Other CSS

--- src/components/VehicleTable.vue
+++ src/components/VehicleTable.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="py-8">
+  <div class="px-8">
+    <h3 class="text-xl font-semibold text-gray-600">Select Vehicle</h3>
+    <p class="text-gray-500 text-sm mb-6">
+      You can select/deselect vehicle from below list by clicking the record it
+      to show/hide the location on Map, if you select a vehicle, it will
+      highlight the vehicle on Map and show its details in the popup. If you
+      click again the selected vehicle, it will deselect the vehicle and remove
+      the highlight from Map.
+    </p>
     <div class="flex justify-between items-center mb-4 gap-3 flex-wrap">
       <FloatLabel>
-        <InputText id="search" v-model="search" />
+        <InputText size="small" id="search" v-model="search" />
         <label for="search">Search</label>
       </FloatLabel>
 
@@ -13,24 +21,25 @@
           optionLabel="label"
           class="w-full md:w-48"
           id="status-select"
+          size="small"
         />
         <label for="status-select">Filter</label>
       </FloatLabel>
     </div>
-    <div class="max-h-[80vh] min-h-[80vh] overflow-auto">
-      <DataView
-        :value="filteredVehicles"
-        :layout="'grid'"
-      >
+    <div class="max-h-[70vh] min-h-[70vh] overflow-auto">
+      <DataView :value="filteredVehicles" :layout="'grid'">
         <template #grid="slotProps">
           <div
             class="p-col-12 md:p-col-4 lg:p-col-3 p-2 cursor-pointer border-x-2 border-b-2 border-gray-200 shadow-sm bg-gray-50"
-            :class="{'border-blue-500 bg-indigo-100': selectedVehicle && selectedVehicle.id === vehicle.id}"
+            :class="{
+              'border-l-4 border-l-indigo-300 bg-indigo-100':
+                selectedVehicle && selectedVehicle.id === vehicle.id,
+            }"
             v-for="vehicle in slotProps.items"
             :key="slotProps.key"
             @click="selectVehicle(vehicle)"
           >
-            <div class="surface-card shadow-2 p-4 border-round">
+            <div class="surface-card shadow-2 p-2 border-round">
               <div class="flex items-center gap-4 mb-2">
                 <div class="text-xl font-semibold">{{ vehicle.name }}</div>
                 <Tag
@@ -46,7 +55,8 @@
                   <i class="pi pi-car mr-2"></i> Type: {{ vehicle.type }}
                 </div>
                 <div class="text-sm mb-1">
-                  <i class="pi pi-map-marker mr-2"></i> {{ vehicle.lastLocation }}
+                  <i class="pi pi-map-marker mr-2"></i>
+                  {{ vehicle.lastLocation }}
                 </div>
                 <div class="text-sm text-color-secondary">
                   <i class="pi pi-clock mr-2"></i>
@@ -103,10 +113,11 @@ const filteredVehicles = computed(() => {
   });
 });
 
-const formatDate = (isoString) => dayjs(isoString).format("DD MMM YYYY, HH:mm A");
+const formatDate = (isoString) =>
+  dayjs(isoString).format("DD MMM YYYY, HH:mm A");
 
 const selectVehicle = (vehicle) => {
-  if(vehicle.id === selectedVehicle.value?.id) {
+  if (vehicle.id === selectedVehicle.value?.id) {
     selectedVehicle.value = null; // Deselect if already selected
   } else {
     selectedVehicle.value = vehicle;
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="max-w-wrapper-md mx-auto px-10 justify-center flex items-center gap-4">
-    <VehicleTable class="w-[40%]" @vehicleSelected="handleVehicleSelection"/>
-    <MapView class="w-[60%]" :selected-vehicle="selectedVehicle"/>
+  <div class="max-w-wrapper-md mx-auto px-10 pt-6">
+    <h1 class="text-2xl font-semibold text-gray-600 mb-6 text-center">
+      Wiot360 Technical Assessment
+    </h1>
+    
+    <div class="justify-center flex items-start gap-4">
+      <VehicleTable class="w-[40%]" @vehicleSelected="handleVehicleSelection" />
+      <MapView class="w-[60%]" :selected-vehicle="selectedVehicle" />
+    </div>
   </div>
 </template>
 
@@ -11,7 +17,4 @@ import MapView from "../components/MapView.vue";
 import { ref } from "vue";
 
 const selectedVehicle = ref(null);
-const handleVehicleSelection = (vehicle) => {
-  selectedVehicle.value = vehicle;
-};
 </script>
